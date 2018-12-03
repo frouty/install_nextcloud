419a9e0931195e1d8f180fee5d4aecaa36626884
Charlie Root <francois.oph@gmail.com>
2018-12-04 05:59:21 +1100

    first commit with install scrip

--- install.nextcloud.sh
+++ install.nextcloud.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
-pkg update
-pkg install nginx mariadb101-server redis php71-bz2 php71-ctype php71-curl php71-dom php71-exif php71-fileinfo php71-filter php71-gd php71-hash 
-php71-iconv php71-intl php71-json php71-mbstring php71-mcrypt php71-pdo_mysql php71-openssl php71-posix php71-session php71-simplexml php71-xml 
-php71-xmlreader php71-xmlwriter php71-xsl php71-wddx php71-zip php71-zlib php71-opcache
+# pkg update
+# pkg install nginx mariadb101-server redis php71-bz2 php71-ctype php71-curl php71-dom php71-exif php71-fileinfo php71-filter php71-gd php71-hash 
+# php71-iconv php71-intl php71-json php71-mbstring php71-mcrypt php71-pdo_mysql php71-openssl php71-posix php71-session php71-simplexml php71-xml 
+# php71-xmlreader php71-xmlwriter php71-xsl php71-wddx php71-zip php71-zlib php71-opcache
+
+
+portsnap fetch extract
+make config-recursive install -C /usr/ports/databases/pecl-redis
+make config-recursive install -C /usr/ports/devel/pecl-APCu
+sysrc 'nginx_enable=YES' 
+sysrc 'php_fpm_enable=YES' 
+sysrc 'mysql_enable=YES' 
+sysrc 'redis_enable=YES'
+cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
