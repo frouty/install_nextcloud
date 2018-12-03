056f309b155942eb0623dca615fff1bb695b1e05
Charlie Root <francois.oph@gmail.com>
2018-12-04 05:47:27 +1100

    first commit with install scrip

---
+++ install.nextcloud.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+pkg update
+pkg install nginx mariadb101-server redis php71-bz2 php71-ctype php71-curl php71-dom php71-exif php71-fileinfo php71-filter php71-gd php71-hash 
+php71-iconv php71-intl php71-json php71-mbstring php71-mcrypt php71-pdo_mysql php71-openssl php71-posix php71-session php71-simplexml php71-xml 
+php71-xmlreader php71-xmlwriter php71-xsl php71-wddx php71-zip php71-zlib php71-opcache
